394df42c02a25b39ec2a8aa8143e2f2f3d236583
lammn <mnlam@lockon-vn.com>
2016-12-06 17:12:57 +0700

    fix best practice

--- ServiceProvider/SalesReportServiceProvider.php
+++ ServiceProvider/SalesReportServiceProvider.php
@@ -14,8 +14,7 @@ use Silex\Application as BaseApplication;
 use Silex\ServiceProviderInterface;
 use Plugin\SalesReport\Service\SalesReportService;
 use Plugin\SalesReport\Form\Type\SalesReportType;
-use Plugin\SalesReport\Utils\Version;
-use Symfony\Component\Translation\Translator;
+use Eccube\Common\Constant;
 
 // include log functions (for 3.0.0 - 3.0.11)
 require_once __DIR__.'/../log.php';
@@ -32,19 +31,26 @@ class SalesReportServiceProvider implements ServiceProviderInterface
      */
     public function register(BaseApplication $app)
     {
+        // 管理画面定義
+        $admin = $app['controllers_factory'];
+        // 強制SSL
+        if ($app['config']['force_ssl'] == Constant::ENABLED) {
+            $admin->requireHttps();
+        }
         // Routingを追加
-        $admin = $app['config']['admin_route'];
-        $app->match($admin.'/sales_report', '\\Plugin\\SalesReport\\Controller\\SalesReportController::index')
-            ->bind('admin_sales_report');
+        $admin->match('/plugin/sales_report', '\\Plugin\\SalesReport\\Controller\\SalesReportController::index')
+            ->bind('admin_plugin_sales_report');
 
-        $app->match($admin.'/sales_report/term', '\\Plugin\\SalesReport\\Controller\\SalesReportController::term')
-            ->bind('admin_sales_report_term');
+        $admin->match('/plugin/sales_report/term', '\\Plugin\\SalesReport\\Controller\\SalesReportController::term')
+            ->bind('admin_plugin_sales_report_term');
 
-        $app->match($admin.'/sales_report/age', '\\Plugin\\SalesReport\\Controller\\SalesReportController::age')
-            ->bind('admin_sales_report_age');
+        $admin->match('/plugin/sales_report/age', '\\Plugin\\SalesReport\\Controller\\SalesReportController::age')
+            ->bind('admin_plugin_sales_report_age');
 
-        $app->match($admin.'/sales_report/product', '\\Plugin\\SalesReport\\Controller\\SalesReportController::product')
-            ->bind('admin_sales_report_product');
+        $admin->match('/plugin/sales_report/product', '\\Plugin\\SalesReport\\Controller\\SalesReportController::product')
+            ->bind('admin_plugin_sales_report_product');
+
+        $app->mount('/'.trim($app['config']['admin_route'], '/').'/', $admin);
 
         // Formの定義
         $app['form.types'] = $app->share($app->extend('form.types', function ($types) use ($app) {
@@ -59,41 +65,35 @@ class SalesReportServiceProvider implements ServiceProviderInterface
         });
 
         // initialize logger (for 3.0.0 - 3.0.8)
-        if (!Version::isSupportGetInstanceFunction()) {
+        if (!method_exists('Eccube\Application', 'getInstance')) {
             eccube_log_init($app);
         }
 
         // メッセージ登録
-        $app['translator'] = $app->share($app->extend('translator', function (Translator $translator, BaseApplication $app) {
-            $file = __DIR__.'/../Resource/locale/message.'.$app['locale'].'.yml';
-            if (file_exists($file)) {
-                $translator->addResource('yaml', $file, $app['locale']);
-            }
-
-            return $translator;
-        }));
+        $file = __DIR__.'/../Resource/locale/message.'.$app['locale'].'.yml';
+        $app['translator']->addResource('yaml', $file, $app['locale']);
 
         // サブナビの拡張
         $app['config'] = $app->share($app->extend('config', function ($config) {
             $nav = array(
-                'id' => 'admin_sales_report',
+                'id' => 'admin_plugin_sales_report',
                 'name' => '売上集計',
                 'has_child' => 'true',
                 'icon' => 'cb-chart',
                 'child' => array(
                     array(
-                        'id' => 'admin_sales_report',
-                        'url' => 'admin_sales_report',
+                        'id' => 'admin_plugin_sales_report',
+                        'url' => 'admin_plugin_sales_report',
                         'name' => '期間別集計',
                     ),
                     array(
-                        'id' => 'admin_sales_report_product',
-                        'url' => 'admin_sales_report_product',
+                        'id' => 'admin_plugin_sales_report_product',
+                        'url' => 'admin_plugin_sales_report_product',
                         'name' => '商品別集計',
                     ),
                     array(
-                        'id' => 'admin_sales_report_age',
-                        'url' => 'admin_sales_report_age',
+                        'id' => 'admin_plugin_sales_report_age',
+                        'url' => 'admin_plugin_sales_report_age',
                         'name' => '年代別集計',
                     ),
                 ),
